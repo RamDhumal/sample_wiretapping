8757a995a0799117f47abbd2e43155f0f0a6ab09
RamDhumal <rajdhumal8011@gmail.com>
2025-08-25 12:37:18 +0530

    Add files via upload

---
+++ sample_intereption/src/main/java/li/interception/InterceptionApplication.java
@@ -0,0 +1,13 @@
+package li.interception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptionApplication.class, args);
+	}
+
+}
---
+++ sample_intereption/src/test/java/li/interception/InterceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package li.interception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
